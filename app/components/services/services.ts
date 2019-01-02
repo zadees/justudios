2cf6b5c804682351ad2b16d8d7090e8d9110eac3
Justin Flowers <justin@justudios.com>
2019-01-02 15:32:26 -0600

    Initial Site Load

---
+++ app/components/services/services.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'services',
+  pipes: [],
+  providers: [],
+  directives: [],
+  styleUrls: ['./services.css'],
+  templateUrl: './services.html'
+})
+export class Services {
+
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+
+}
